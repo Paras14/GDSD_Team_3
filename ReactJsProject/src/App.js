13e8f829c115e0eb00186e9047fa159eaabc280f
NOMAN ALI <87797326+nomansahi@users.noreply.github.com>
2022-11-20 14:23:37 +0100

    initial commit

---
+++ ReactJsProject/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import About from "./components/about";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Team 3</h1>
+
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJsProject/src/components/about.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Our taem</h1>
+    </div>
+  );
+};
+
+export default About;
