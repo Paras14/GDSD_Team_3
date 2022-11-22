137e51a34f8e2a395f48536847efc178b9341e8a
Luis Miguel García <linten42@gmail.com>
2022-11-22 18:25:33 +0100

    Add About information in ReactProject

--- ReactJsProject/src/App.js
+++ ReactJsProject/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import About from "./components/about";
+import React from "react";
 
 function App() {
   return (
--- ReactJsProject/src/components/about.jsx
+++ ReactJsProject/src/components/about.jsx
@@ -1,9 +1,79 @@
 import React, { Component } from "react";
+import "../styles/about.css";
+import parasIMG from "../static/Paras_img.png";
+import vichitarIMG from "../static/Photo_Vichitar.jpg";
 
 const About = () => {
   return (
     <div>
-      <h1>About Our taem</h1>
+      <h1>About Our Team</h1>
+      <div class="header">Landing Page Milestone_0</div>
+      <div class="content">
+        <h1>Meet our Team</h1>
+        <div class="container">
+          <div class="flex-items">
+            <a href="http://team3gdsdweb.s3-website-us-east-1.amazonaws.com/personal_webpages/Luis-site.html"
+            >
+              <img
+                src="https://avatars.githubusercontent.com/u/74145538?v=4"
+                alt="Luis_image"
+              />
+            </a>
+            <div class="info">Luis</div>
+          </div>
+          <div class="flex-items">
+            <a href="http://team3gdsdweb.s3-website-us-east-1.amazonaws.com/personal_webpages/ABOUT_Jesus.html"
+              ><img
+                src="https://avatars.githubusercontent.com/u/74461667?v=4"
+                alt="Jesus_image"
+              />
+            </a>
+            <div class="info">Jesús</div>
+          </div>
+          <div class="flex-items">
+            <a href="http://team3gdsdweb.s3-website-us-east-1.amazonaws.com/personal_webpages/Noman.html"
+              ><img
+                src="https://avatars.githubusercontent.com/u/87797326?v=4"
+                alt="Noman_image"
+              />
+            </a>
+            <div class="info">Noman</div>
+          </div>
+          <div class="flex-items">
+            <a href="http://team3gdsdweb.s3-website-us-east-1.amazonaws.com/personal_webpages/Paras_personal-site.html"
+              ><img
+                src={parasIMG}
+                alt="Paras_image"
+              />
+            </a>
+            <div class="info">Paras</div>
+          </div>
+          <div class="flex-items">
+            <a href="http://team3gdsdweb.s3-website-us-east-1.amazonaws.com/personal_webpages/Vichitar_About_Page.html"
+              ><img
+                src={vichitarIMG}
+                alt="Vichitar_image"
+              />
+            </a>
+            <div class="info">Vichitar</div>
+          </div>
+          <div class="flex-items">
+            <a href="http://team3gdsdweb.s3-website-us-east-1.amazonaws.com/personal_webpages/aboutHassan.html"
+              ><img
+                src="https://avatars.githubusercontent.com/u/95298592?v=4"
+                alt="Hassan_image"
+              />
+            </a>
+            <div class="info">Hassan</div>
+          </div>
+        </div>
+      </div>
+      <div class="links">
+        Github : - ><a href="https://github.com/Paras14/GDSD_Team_3"
+          >GDSD_Team_3</a
+        >
+      </div>
+      <div class="footer">Made with teamwork</div>
     </div>
   );
 };
