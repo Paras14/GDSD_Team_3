91704b2254f8f76ddcf8fd84ebc3174107080d39
NOMAN ALI <87797326+nomansahi@users.noreply.github.com>
2023-01-08 12:33:45 +0100

    get data from sign in form and show in console

--- ReactJsProject/src/components/SignIn/SignIn.jsx
+++ ReactJsProject/src/components/SignIn/SignIn.jsx
@@ -1,20 +1,43 @@
 import { useNavigate } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
+
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import { useState } from "react";
 
 function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
+
   return (
     <Container>
       <h1 className="text-center my-4 py-4">Please Login Here!</h1>
-      <Form>
+
+      <Form onSubmit={handleSubmit}>
         <Row>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              name="email"
+              onChange={handleEmailChange}
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -22,7 +45,12 @@ function SignIn() {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handlePasswordChange}
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
