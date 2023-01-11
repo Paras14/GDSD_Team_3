428fda5515af09325c53a6aff69679b1ac687d14
NOMAN ALI <87797326+nomansahi@users.noreply.github.com>
2023-01-11 23:03:58 +0100

    Add Pending restaurent registration

--- ReactJsProject/src/components/Admin/PendingRestaurentForm.jsx
+++ ReactJsProject/src/components/Admin/PendingRestaurentForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Row, Col, Button } from "react-bootstrap";
 
 function PendingRestaurentForm() {
   const restaurentData = [
@@ -17,17 +17,81 @@ function PendingRestaurentForm() {
   return (
     <Container>
       <div>
-        <h1>PENDING RESTAURANT REGISTRATIONS</h1>
+        <h1 className="text-center fs-4 p-3 m-3">
+          PENDING RESTAURANT REGISTRATIONS
+        </h1>
         <div>
           {restaurentData.map((data) => {
             return (
-              <Card>
-                <Card body>{data.restaurentName}</Card>
-                <Card body>{data.managerName}</Card>
-              </Card>
+              <div>
+                <Row className="mb-3">
+                  <Card body as={Col} md="4">
+                    <p>
+                      <span className="fs-4 p-2">Restaurent Name:</span>
+                      {data.restaurentName}
+                    </p>
+                  </Card>
+
+                  <Card body as={Col} md="4">
+                    <p>
+                      <span className="fs-4 p-2">Manager Name:</span>
+                      {data.managerName}
+                    </p>
+                  </Card>
+
+                  <Card body as={Col} md="4">
+                    <p>
+                      <span className="fs-4 p-2">Manager Email:</span>
+                      {data.managerEmail}
+                    </p>
+                  </Card>
+                </Row>
+
+                <Row className="mb-3">
+                  <Card body as={Col} md="4">
+                    <p>
+                      <span className="fs-4 p-2">City:</span>
+                      {data.city}
+                    </p>
+                  </Card>
+                  <Card body as={Col} md="4">
+                    <p>
+                      <span className="fs-4 p-2">State:</span>
+                      {data.state}
+                    </p>
+                  </Card>
+                  <Card body as={Col} md="4">
+                    <p>
+                      <span className="fs-4 p-2">Phone Number:</span>
+                      {data.managerTellophone}
+                    </p>
+                  </Card>
+                </Row>
+
+                <Row className="mb-3">
+                  <Card body as={Col} md="6">
+                    <p>
+                      <span className="fs-4 p-2">Zip Code:</span>
+                      {data.zipCode}
+                    </p>
+                  </Card>
+                  <Card body as={Col} md="6">
+                    <p>
+                      <span className="fs-4 p-2">Restaurent Type:</span>
+                      {data.restaurentType}
+                    </p>
+                  </Card>
+                </Row>
+              </div>
             );
           })}
         </div>
+        <Row>
+          <Button className="mb-3" size="lg">
+            Decline
+          </Button>
+          <Button size="lg">Accept</Button>
+        </Row>
       </div>
     </Container>
   );
