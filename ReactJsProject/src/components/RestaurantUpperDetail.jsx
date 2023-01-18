cf73b93b1305e9fe4423265eb50f2302f2f3586c
Paras <parasv249@gmail.com>
2023-01-18 19:45:02 +0100

    Update RestaurantUpperDetail

--- ReactJsProject/src/components/RestaurantUpperDetail.jsx
+++ ReactJsProject/src/components/RestaurantUpperDetail.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { Button, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 // import ListOfRestaurants from "./Admin/ListOfRestaurants";
+import imageRestaurant from "../images/RestaurantUpperImage.jpg"
 
 function RestaurantUpperDetail() {
   const navigate = useNavigate();
 //   const arrow = <FontAwesomeIcon icon={faArrowRight} />;
   const restaurantDetail = [
-    { name: "Restaurant 1", info: "The restaurant is located in the quiet streets of the historic old town of Fulda. A special experience: The cozy restaurant, in summer with a wonderful street terrace, friendly staff and delicious dishes from regional and Mediterranean cuisine.user1@gmail.com" }];
+    { name: "Restaurant 1", info: "The restaurant is located in the quiet streets of the historic old town of Fulda. A special experience: The cozy restaurant, in summer with a wonderful street terrace, friendly staff and delicious dishes from regional and Mediterranean cuisine.user1@gmail.com", imgSource: imageRestaurant}];
   return (
     <Container>
       <div class=".restaurant-photo">
-        <img href="https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg"></img>
+        <img href={restaurantDetail[0].imgSource}/>
         <p>{restaurantDetail[0].name}</p>
         <p>{restaurantDetail[0].info}</p>
         <Button
