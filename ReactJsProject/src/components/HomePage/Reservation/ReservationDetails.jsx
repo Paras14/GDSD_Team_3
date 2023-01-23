33e4aa78bde614409b77dcb8a717a12ee5b7c0d2
Paras <parasv249@gmail.com>
2023-01-23 20:54:56 +0100

    Add all inputs

--- ReactJsProject/src/components/HomePage/Reservation/ReservationDetails.jsx
+++ ReactJsProject/src/components/HomePage/Reservation/ReservationDetails.jsx
@@ -7,20 +7,20 @@ import RestaurantPhoto from "../../RestaurantPhoto";
 function ReservationDetails() {
   const navigate = useNavigate();
 //   const arrow = <FontAwesomeIcon icon={faArrowRight} />;
-  //const restaurantDetail = [
-    //{ name: "Restaurant 1", info: "The restaurant is located in the quiet streets of the historic old town of Fulda. A special experience: The cozy restaurant, in summer with a wonderful street terrace, friendly staff and delicious dishes from regional and Mediterranean cuisine.user1@gmail.com"}];
+  const restaurantDetail = [
+    { name: "Restaurant 1", info: "The restaurant is located in the quiet streets of the historic old town of Fulda. A special experience: The cozy restaurant, in summer with a wonderful street terrace, friendly staff and delicious dishes from regional and Mediterranean cuisine.user1@gmail.com"}];
   return (
     <Container>
-    <br></br>
       <Row>
+        <h2>Reservation Page</h2>
         <RestaurantPhoto />
+        <h4>{restaurantDetail[0].name}</h4>
       </Row>
-      <br></br>
       <Row>
         <Container>
           <Row>
-            <Col className="col-lg-4">
-            <label for="reservation-date">Date and Hour:</label>
+            <Col className="col-lg-3">
+            <label for="reservation-date">Date and Hour:</label><br/>
             <input type="date" id="reservation-date" name="reservation-date" required />
             </Col>
             <Col className="col-lg-3">
@@ -30,6 +30,42 @@ function ReservationDetails() {
             <Col className="col-lg-3">
               <label for="people_number">Number of People:</label>
               <input type="text" id="people_number" name="people_number" required />
+            </Col>
+          </Row>
+          <hr></hr>
+          <Row>
+            <Col className="col-lg-4">
+            <Button
+              size="lg"
+              className="text-center"
+              onClick={() => {
+                  navigate("/");
+              }}
+              >
+              Select table
+            </Button>
+            </Col>
+            <Col className="col-lg-4">
+            <Button
+              size="lg"
+              className="text-center"
+              onClick={() => {
+                  navigate("/");
+              }}
+              >
+              Select Food
+            </Button>
+            </Col>
+            <Col className="col-lg-4">
+            <Button
+              size="lg"
+              className="text-center"
+              onClick={() => {
+                  navigate("/");
+              }}
+              >
+              Parking
+            </Button>
             </Col>
           </Row>
         </Container>
