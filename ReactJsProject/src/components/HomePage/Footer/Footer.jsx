8e79e792875e99157cdb70dbbca81eded7a13b42
Luis Miguel García <linten42@gmail.com>
2023-01-25 20:06:53 +0100

    Update Footer.jsx

--- ReactJsProject/src/components/HomePage/Footer/Footer.jsx
+++ ReactJsProject/src/components/HomePage/Footer/Footer.jsx
@@ -3,10 +3,10 @@ import { Container } from "react-bootstrap";
 
 function Footer() {
   return (
-    <div className="bg-primary  text-white fixed-bottom">
+    <div className="bg-primary py-1 text-white static-bottom">
       <Container>
         <div>
-          <p className="text-center fs-4 mt-2">
+          <p className="text-center fs-4">
             &copy; <span className="fw-bold">Risto.</span> Fulda University of
             Applied Sciences Software Engineering Project, Fall 2022 For
             Demonstration Only
