bf0c8b4820b4ce3a8d3c822492a7238a92eb7887
NOMAN ALI <87797326+nomansahi@users.noreply.github.com>
2023-01-25 19:33:06 +0100

    create review page

--- ReactJsProject/src/components/RestaurantTabDetails.jsx
+++ ReactJsProject/src/components/RestaurantTabDetails.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Container, Button } from "react-bootstrap";
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Global } from "../helpers/Global.js";
+import Review from "./Reservations/Review.jsx";
+import PendingReviews from "./Admin/PendingReviews.jsx";
+import PendingReviewPost from "./Admin/PostReviews/PendingReviewPost.jsx";
 
-function RestaurantTabDetails({restaurantDetail}) {
-
+function RestaurantTabDetails({ restaurantDetail }) {
   const baseUrl = Global.baseUrl;
   const [foods, setFoods] = useState([]);
 
   useEffect(() => {
-    
     if (restaurantDetail !== null) {
-
       // Get foods from the restaurant
       axios
         .get(baseUrl + "foods/restaurant/" + restaurantDetail.id)
@@ -25,12 +25,10 @@ function RestaurantTabDetails({restaurantDetail}) {
         .catch((error) => {
           console.log(error);
         });
-
     }
-
   }, []);
 
-    return (
+  return (
     <Container>
       <Tabs
         defaultActiveKey="profile"
@@ -38,26 +36,29 @@ function RestaurantTabDetails({restaurantDetail}) {
         className="mb-3"
       >
         <Tab eventKey="Info" title="Info">
-          <p>{ restaurantDetail.description }</p>
-          <p>Telephone: { restaurantDetail.telephone } </p>
-          <p>State: { restaurantDetail.state } </p>
-          <p>City: { restaurantDetail.city } </p>
-          <p>ZIP: { restaurantDetail.zip } </p>
+          <p>{restaurantDetail.description}</p>
+          <p>Telephone: {restaurantDetail.telephone} </p>
+          <p>State: {restaurantDetail.state} </p>
+          <p>City: {restaurantDetail.city} </p>
+          <p>ZIP: {restaurantDetail.zip} </p>
         </Tab>
         <Tab eventKey="Menu" title="Menu">
-          <p> {foods.length !== 0 ?
-              foods.toString()
-            : "This restaurant has no uploaded menu." } </p>
+          <p>
+            {" "}
+            {foods.length !== 0
+              ? foods.toString()
+              : "This restaurant has no uploaded menu."}{" "}
+          </p>
         </Tab>
         <Tab eventKey="Review" title="Review">
-          <p> This is the Review Section</p>
+          <Review />
         </Tab>
         <Tab eventKey="Parking" title="Parking">
           <p> This Restaurant has Parking Facilities Section</p>
         </Tab>
       </Tabs>
-      </Container>
-    );
-  }
-  
-  export default RestaurantTabDetails;
+    </Container>
+  );
+}
+
+export default RestaurantTabDetails;
