db24992936476dd1345cf7d1fec849bbed01729c
jesu-m0 <jesusmd2000@gmail.com>
2023-01-26 15:01:34 +0100

    Update Footer.jsx

--- ReactJsProject/src/components/HomePage/Footer/Footer.jsx
+++ ReactJsProject/src/components/HomePage/Footer/Footer.jsx
@@ -3,18 +3,6 @@ import { Container } from "react-bootstrap";
 
 function Footer() {
   return (
-<<<<<<< Updated upstream
-    <div className="bg-primary py-1 text-white static-bottom">
-      <Container>
-        <div>
-          <p className="text-center fs-4">
-            &copy; <span className="fw-bold">Risto.</span> Fulda University of
-            Applied Sciences Software Engineering Project, Fall 2022 For
-            Demonstration Only
-          </p>
-        </div>
-      </Container>
-=======
     <div>
       <br></br>
       <br></br>
@@ -29,7 +17,6 @@ function Footer() {
           </div>
         </Container>
       </div>
->>>>>>> Stashed changes
     </div>
   );
 }
