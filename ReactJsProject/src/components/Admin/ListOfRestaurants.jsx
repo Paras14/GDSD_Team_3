281b01dbda066af2e3b3c12e4032f69872a36edc
Paras <parasv249@gmail.com>
2023-01-29 18:51:54 +0100

    Connect with backend

--- ReactJsProject/src/components/Admin/ListOfRestaurants.jsx
+++ ReactJsProject/src/components/Admin/ListOfRestaurants.jsx
@@ -1,42 +1,62 @@
 import React from "react";
+import axios from "axios";
 import { Button, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function ListOfRestaurants() {
   const navigate = useNavigate();
+  const [pendingRestaurant, setPendingRestaurant] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://admin/petitions/restaurant/pending")
+      .then((res) => {
+        setPendingRestaurant(res.data);
+        if (res.data.status === "" || res.data.status === "accepted") {
+          return <p>There are no Restaurants for review</p>;
+        } else {
+          return null;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   //   const arrow = <FontAwesomeIcon icon={faArrowRight} />;
-  const pendingData = [
-    { name: "Restaurant 1", email: "Restaurant1@gmail.com" },
-    { name: "Restaurant 2", email: "Restaurant2@gmail.com" },
-    { name: "Restaurant 3", email: "Restaurant3@gmail.com" },
-    { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
-    { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
-    { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
-    { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
-    { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
-    { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
-    { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
-    { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
-    { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
-    { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
-    { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
-    { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
-    { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
-    { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
-    { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
-    { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
-    { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
-    { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
-    { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
-    { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
-    { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
-    { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
-    { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
-    { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
-    { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
-    { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
-    { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
-  ];
+  // const pendingData = [
+  //   { name: "Restaurant 1", email: "Restaurant1@gmail.com" },
+  //   { name: "Restaurant 2", email: "Restaurant2@gmail.com" },
+  //   { name: "Restaurant 3", email: "Restaurant3@gmail.com" },
+  //   { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
+  //   { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
+  //   { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
+  //   { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
+  //   { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
+  //   { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
+  //   { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
+  //   { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
+  //   { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
+  //   { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
+  //   { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
+  //   { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
+  //   { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
+  //   { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
+  //   { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
+  //   { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
+  //   { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
+  //   { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
+  //   { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
+  //   { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
+  //   { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
+  //   { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
+  //   { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
+  //   { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
+  //   { name: "Restaurant 4", email: "Restaurant4@gmail.com" },
+  //   { name: "Restaurant 5", email: "Restaurant5@gmail.com" },
+  //   { name: "Restaurant 6", email: "Restaurant6@gmail.com" },
+  // ];
   return (
     <Container>
       <div>
@@ -44,7 +64,7 @@ function ListOfRestaurants() {
           List of Restaurants
         </p>
         <div style={{ overflow: "scroll", maxHeight: "25rem" }}>
-          {pendingData.map((data) => {
+          {pendingRestaurant.map((data) => {
             return (
               <div className="d-flex justify-content-between mb-2 p-3 m-2 bg-light rounded">
                 <p className="fs-4 fw-bold">{data.name}</p>
