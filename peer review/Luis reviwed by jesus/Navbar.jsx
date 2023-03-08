41269f86d2ee6801b96fb05f5bc70e1462b9b7c3
jesu-m0 <jesusmd2000@gmail.com>
2023-03-08 17:50:40 +0100

    Chat.controller reviwed

---
+++ peer review/Luis reviwed by jesus/Navbar.jsx
@@ -0,0 +1,139 @@
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import "../HomePage/navbarIndex.css";
+import Logo from "./Logo";
+import { isAuthorized } from "../../helpers/isAuthorized";
+import { useState, useEffect } from "react";
+
+const Navbar = () => {
+  const isauthorized = isAuthorized();
+  const [user, setUser] = useState(null);
+
+  const element = <FontAwesomeIcon icon={faUser} />;
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Get user from local storage
+    if (isauthorized) {
+      setUser(JSON.parse(localStorage.getItem("user")));
+    }
+  }, []);
+
+  return (
+    user !== null // if user is logged in 
+      ?
+        <div className=" bg-primary ">
+          <div className="container ">
+            <div className="d-flex justify-content-between ">
+              <div
+                onClick={() => {
+                  if (user.rolId === 8) { // 8 is for customer
+                    navigate("/");
+                  } else if (user.rolId === 7) { // 7 is for admin
+                    navigate("/adminPanel");
+                  } else if(user.rolId === 9){ // 9 is for manager
+                    navigate("/managerPanel");
+                  }
+                  
+                }}
+              >
+                <Logo fill="white" />
+              </div>
+            
+              {user.rolId === 7 ? // 7 is for admin
+                <div className="align-self-center">
+                  <p className="text-light fs-3">Admin Panel</p>
+                </div>
+                :
+                  null}
+
+              {user.rolId === 9 ? // 9 is for manager
+                <div className="align-self-center">
+                  <p className="text-light fs-3">Manager Panel</p>
+                </div>
+                
+                :
+                  null}  
+             
+              <div className="align-self-center d-flex text-light">
+
+                {user.rolId === 8 ? // 8 is for customer
+                
+                  <button
+                  className="btn btn-outline-light me-2"
+                  onClick={() => {
+                    navigate("/reservations");
+                  }}
+                  >
+                    Reservations
+                  </button>
+                  :
+                  null
+                }
+                
+                <button class="btn btn-outline-light me-2" type="button" onClick={() => {
+                    navigate("/profile");
+                 }}>{element}<span className="ps-2">{ user !== null ? user.username : "User"}</span>
+                </button>
+                <div className="bg-light rounded">
+                  <button
+                    className="btn btn-outline-secondary"
+                    onClick={() => {
+                      localStorage.clear();
+                      navigate("/");
+                      window.location.reload();
+                    }}
+                  >
+                    Logout
+                  </button>
+                </div>
+                <a href="/about" className="btn btn-secondary ms-2">
+                  About us
+                </a>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        : // if user is not logged in
+        <div className=" bg-primary ">
+          <div className="container ">
+            <div className="d-flex justify-content-between ">
+              <div
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                <Logo fill="white" />
+              </div>
+              <div className="align-self-center">
+                
+                <button
+                  className="btn btn-outline-light mx-2"
+                  onClick={() => {
+                    navigate("/signIn");
+                  }}
+                >
+                  Log in
+                </button>
+                <button
+                  className="btn btn-light me-2"
+                  onClick={() => {
+                    navigate("/userType");
+                  }}
+                >
+                  Register
+                </button>
+                <a href="/about" className="btn btn-secondary">
+                  About us
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>       
+  );
+};
+
+export default Navbar;
---
+++ peer review/Luis reviwed by jesus/chats.controller.js
@@ -0,0 +1,195 @@
+const db = require("../models");
+const Chat = db.chat;
+const Op = db.Sequelize.Op;
+
+//JESUS REVIEW: In all methods if there is an error you send a very generic message "Some errors..."
+//you could control at least the error if you can found a object in the database.
+//Example:
+/*
+db.sequelize.query("SELECT * FROM users u WHERE u.id IN (SELECT DISTINCT user_receiver FROM chats WHERE user_emitter = :user UNION SELECT DISTINCT user_emitter FROM chats WHERE user_receiver = :user)", {
+        replacements: { user: userid },
+        type: db.sequelize.QueryTypes.SELECT,
+      })
+        .then(users => {
+    ->    if(users){
+            res.send(users);
+          }else{
+    ->      res.status(404).send({
+    ->         message: `Cannot get users with id=${userid}.`
+            });
+          }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                err.message || "Some error occurred while retrieving users."
+            });
+        });
+
+        With that you are sending a more specific message to the user. In some methods you use this, in others not.
+*/
+
+// Create and Save a new Chat (message of a conversation)
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.user_emitter || !req.body.user_receiver) {
+      res.status(400).send({
+        message: "Chat must have sender and receiver id"
+      });
+      return;
+    }
+  
+    // Create a chat
+    const chat = {
+        user_emitter: req.body.user_emitter, //JESUS: we have used camelCase in the project so we should use it here too i think. 
+        user_receiver: req.body.user_receiver,// same here as above
+        text: req.body.text
+    };
+  
+    // Save Chat in the database
+    Chat.create(chat)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Chat."
+        });
+      });
+  };
+
+// Retrieve all users which have a chat with the user with the id: userid
+exports.findAllConversationsFromUser = (req, res) => {
+    const userid = req.params.userid;
+
+    db.sequelize.query("SELECT * FROM users u WHERE u.id IN (SELECT DISTINCT user_receiver FROM chats WHERE user_emitter = :user UNION SELECT DISTINCT user_emitter FROM chats WHERE user_receiver = :user)", {
+        replacements: { user: userid },
+        type: db.sequelize.QueryTypes.SELECT,
+      })
+        .then(users => {
+          res.send(users);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                err.message || "Some error occurred while retrieving users."
+            });
+        });
+
+    };
+
+// Retrieve all Chats between 2 users from the database.
+exports.findAllByConversation = (req, res) => {
+    const userid1 = req.query.userid1; //JESUS: we have used camelCase in the project so we should use it here too i think.
+    const userid2 = req.query.userid2; // same here as above
+
+    Chat.findAll({
+        where: {
+            [Op.or]: [
+                { user_emitter: userid1, user_receiver: userid2 },
+                { user_emitter: userid2, user_receiver: userid1 }
+            ]
+        }
+    }).then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+            err.message || "Some error occurred while retrieving chats."
+        });
+    });
+      
+  };
+
+// Find a single Chat with an id
+exports.findOne = (req, res) => {
+    const id = req.query.id;
+  
+    Chat.findByPk(id)
+      .then(data => {
+        if (data) {
+          res.send(data);
+        } else {
+          res.status(404).send({
+            message: `Cannot find Chat with id=${id}.` //JESUS: Great! Here you are sending a more specific message to the user.
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error retrieving Chat with id=" + id
+        });
+      });
+  };
+
+  //JESUS: this method is perfect! Good job!
+// Update a Chat by the id in the request
+exports.update = (req, res) => {
+    const id = req.query.id;
+    
+    Chat.update(req.query, {
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Chat was updated successfully."
+          });
+        } else {
+          res.send({
+            message: `Cannot update Chat with id=${id}. Maybe Chat was not found or req.query is empty!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating Chat with id=" + id
+        });
+      });
+  };
+
+//JESUS: this method is perfect! Good job!
+// Delete a Chat with the specified id in the request
+exports.delete = (req, res) => {
+    const id = req.query.id;
+  
+    Chat.destroy({
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Chat was deleted successfully!"
+          });
+        } else {
+          res.send({
+            message: `Cannot delete Chat with id=${id}. Maybe Chat was not found!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Could not delete Chat with id=" + id
+        });
+      });
+  };
+
+// Delete all Chats from the database.
+exports.deleteAll = (req, res) => {
+    Chat.destroy({
+      where: {},
+      truncate: false
+    })
+      .then(nums => {
+        res.send({ message: `${nums} Chats were deleted successfully!` });
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while removing all chats."
+        });
+      });
+  };
+
